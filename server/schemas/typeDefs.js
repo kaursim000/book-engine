be042c118a6cdfff772ab5bd6ba646f7426ff4c1
Simran Kaur <kaursim.2019@yahoo.com>
2021-08-27 20:28:06 -0700

    typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: Number
+    savedBooks: 
+  }
+
+  type Book {
+    bookId: ID
+    description: String
+    authors: String[]
+    createdAt: String
+    comments: [Comment]!
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addBook(thoughtText: String!): Book
+    removeBook(bookId: ID!): Book
+  }
+`;
+
+module.exports = typeDefs;
